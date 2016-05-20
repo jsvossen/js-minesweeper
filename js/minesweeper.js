f13e0fb3b7e0ffdd36050a0afbf89f8f70cbbecb
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-05-20 16:02:37 -0400

    populate mines

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -12,9 +12,35 @@
 					thisGrid.data.push([]);
 					for (col=0; col < thisGrid.size; col++) {
 						$(rowDiv).append('<div class="cell" data-col='+col+'></div>');
-						thisGrid.data[row].push("");
+						thisGrid.data[row].push(0);
 					}
 				}		
+			},
+			populate: function(mines) {
+				var i = 0;
+				while (i<mines) {
+					var row = Math.floor( Math.random() * this.size );
+					var col = Math.floor( Math.random() * this.size );
+					if ( !this.data[row][col]) {
+						this.data[row][col] = 1;
+						i++;
+					}
+				}
+			},
+			getCell: function(row,col) {
+				return $('.row[data-row='+row+'] .cell[data-col='+col+']');
+			},
+			getCoords: function(cell) {
+				var row = $(cell).parent().attr('data-row');
+				var col = $(cell).attr('data-col');
+				return {
+					row: row,
+					col: col
+				}
+			},
+			hasMine: function(cell) {
+				coords = this.getCoords(cell);
+				return this.data[coords.row][coords.col];
 			},
 			flagCell: function(cell) {
 				if ( !$(cell).hasClass('cleared') ) {
@@ -24,6 +50,9 @@
 			clearCell: function(cell) {
 				if ( !$(cell).hasClass('flag') ) {
 					$(cell).addClass('cleared');
+					if ( this.hasMine(cell) ) {
+						$(cell).addClass('mine');
+					}
 				}
 			}
 		}
@@ -32,6 +61,7 @@
 			data: grid.data,
 			init: function() { 
 				grid.init();
+				grid.populate(10);
 				$('.cell').on("contextmenu", function(event){
 					event.preventDefault();
 					grid.flagCell(this);
